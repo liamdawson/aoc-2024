202d856c7f215da89aa2052c1e0196701601f50e
Liam Dawson <liam@ldaws.com>
2024-12-17 17:49:40 +1100

    Refactor day 1 logic

---
+++ solutions/1/common.ts
@@ -0,0 +1,28 @@
+export async function readInputFile(path: string): Promise<string[]> {
+  return (await Deno.readTextFile(path)).split(/\r?\n/);
+}
+
+const extractorRegex = /^\s*(\d+)\s+(\d+)\s*$/;
+export function getSortedColumns(contentLines: string[]): [number[], number[]] {
+  const lists: [number[], number[]] = [[], []];
+  const addValues = (values: string[]) => {
+    lists[0].push(Number(values[0]));
+    lists[1].push(Number(values[1]));
+  };
+
+  for (const line of contentLines) {
+    const extracted = extractorRegex.exec(line);
+    if (!extracted) {
+      continue;
+    }
+    const [_line, ...values] = extracted;
+
+    addValues(values);
+  }
+
+  for (const list of lists) {
+    list.sort((a, b) => a - b);
+  }
+
+  return lists;
+}
--- solutions/1/p1.ts
+++ solutions/1/p1.ts
@@ -1,34 +1,13 @@
-const contentLines = (await Deno.readTextFile(Deno.args[0] ?? "input.txt"))
-  .split(
-    /\r?\n/,
-  );
+import { getSortedColumns, readInputFile } from "./common.ts";
 
-const lists: [number[], number[]] = [[], []] as const;
-
-const extractorRegex = /^\s*(\d+)\s+(\d+)\s*$/;
-for (const line of contentLines) {
-  const extracted = extractorRegex.exec(line);
-  if (!extracted) {
-    continue;
-  }
-  const [_line, ...values] = extracted;
-
-  for (const [i, list] of lists.entries()) {
-    list.push(Number(values[i]));
-  }
-}
-
-for (const list of lists) {
-  list.sort((a, b) => a - b);
-}
+const contentLines = await readInputFile(Deno.args[0] ?? "input.txt");
+const [firstColumn, secondColumn] = getSortedColumns(contentLines);
 
 let totalDistance = 0;
-const firstList = lists[0];
-const secondList = lists[1];
-for (let i = 0; i < firstList.length; i++) {
-  totalDistance += Math.abs(secondList[i] - firstList[i]);
+for (let i = 0; i < firstColumn.length; i++) {
+  totalDistance += Math.abs(secondColumn[i] - firstColumn[i]);
 }
 
 console.log(
-  `Total distance between ${firstList.length} pairs: ${totalDistance}`,
+  `Total distance between ${firstColumn.length} pairs: ${totalDistance}`,
 );
