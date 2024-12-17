86b8e59a0f0775c0cc6f701553f69b5b7407d368
Liam Dawson <liam@ldaws.com>
2024-12-17 23:55:16 +1100

    Complete D1P2

--- solutions/1/p2.ts
+++ solutions/1/p2.ts
@@ -1,8 +1,34 @@
 import { getSortedColumns, readInputFile } from "./common.ts";
 
+const singleSimilarityScore = (id: number, count: number) => id * count;
+
 export default async function main(args: typeof Deno.args) {
   const contentLines = await readInputFile(args[0] ?? "input.txt");
   const [firstColumn, secondColumn] = getSortedColumns(contentLines);
+
+  const idScores = new Map<number, number>();
+
+  let lastId = secondColumn[0];
+  let idCount = 0;
+  for (const id of secondColumn) {
+    if (lastId === id) {
+      idCount += 1;
+      continue;
+    }
+
+    idScores.set(lastId, idCount);
+    lastId = id;
+    idCount = 1;
+  }
+
+  idScores.set(lastId, idCount);
+
+  let similarityScore = 0;
+  for (const id of firstColumn) {
+    similarityScore += singleSimilarityScore(id, idScores.get(id) ?? 0);
+  }
+
+  console.log(`Similarity score: ${similarityScore}`);
 }
 
 if (import.meta.main) {
