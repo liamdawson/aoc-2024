d2fadec72f2ceb25a52fafd7f238b35eedcacf04
Liam Dawson <liam@ldaws.com>
2024-12-18 14:15:03 +1100

    Working D3P1

--- solutions/3/p1.ts
+++ solutions/3/p1.ts
@@ -1,7 +1,15 @@
 import { linesFromInputFile } from "#shared";
+import { evaluateInstruction, extractInstructions } from "./common.ts";
 
 export default async function main(args: string[]) {
-  const _inputLines = await linesFromInputFile(args[0] ?? "input.txt");
+  const inputLines = await linesFromInputFile(args[0] ?? "input.txt");
+  const instructions = inputLines.flatMap((line) => extractInstructions(line));
+
+  const result = instructions
+    .map((instruction) => evaluateInstruction(instruction))
+    .reduce((a, b) => a + b);
+
+  console.log(`Sum of ${instructions.length} instructions = ${result}`);
 }
 
 if (import.meta.main) {
